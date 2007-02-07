e838c6fc86e5c173487f6ccc14a748a1f090ce07
edorka <edorka@fd8f7eb6-0621-0410-b421-4720bd429cc4>
2007-02-07 10:47:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/prueba1.java

---
+++ src/prueba1.java
@@ -0,0 +1,11 @@
+﻿public void () ;
+
+String cadena = a+b*c;
+int operando = - cadena / objeto.metodo(comando1, comando2);
+void unaFuncion () {
+    for (i=0; i<=28; i++) {
+        /* De momento no hago nada */
+        h*=i + -.023E+9;
+    }
+    return -1;
+}

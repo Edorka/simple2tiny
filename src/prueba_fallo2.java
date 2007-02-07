b92f3717864485dbd3b775830f82a0a6f9e611db
edorka <edorka@fd8f7eb6-0621-0410-b421-4720bd429cc4>
2007-02-07 10:48:22 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/prueba_fallo2.java

---
+++ src/prueba_fallo2.java
@@ -0,0 +1,9 @@
+﻿/* Comentarios sin cerrar y variables incorrectas */
+
+variable1 = 1variable;
+variable2 = _variable1;
+int entero = 1a2 + -73;
+int otro_entero = +123a;
+
+/* Comentario multilinea sin cerrar
+

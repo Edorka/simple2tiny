800132b1eb6d6729b5796b83fafe8a7327761087
edorka <edorka@fd8f7eb6-0621-0410-b421-4720bd429cc4>
2007-02-07 10:48:39 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/prueba_incorrecta_constantes.java

---
+++ src/prueba_incorrecta_constantes.java
@@ -0,0 +1,6 @@
+﻿/* Prueba de constantes numericas mal formadas */
+int valor_malo = +28.3
+int valor_malo_2 = .002E13
+int valor_malo_3 = 123e84
+int valor_malo_5 = 1_234
+int valor_malo_6 = 12356_

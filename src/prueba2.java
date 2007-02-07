55ed677a2de2bf9ea92acb4d229fe2d7accc9606
edorka <edorka@fd8f7eb6-0621-0410-b421-4720bd429cc4>
2007-02-07 10:48:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/prueba2.java

---
+++ src/prueba2.java
@@ -0,0 +1,24 @@
+﻿// Prueba de comentario de una linea
+
+if (!tengo_pelo) {
+    objeto.imprime(calvo);
+}
+
+a++;
+b--;
+c*=a;
+d/=a;
+d=+.03E-10;
+
+/* Pruebas de operadores logicos
+****
+*/
+
+/* Comentario extra
+
+
+*/
+
+boolean valor = (a && b) || z;
+boolean otro_valor = (calvo == a);
+boolean otro_valor_mas = (calvo != z);

958b75325f6c1432d7550b3af517601b22e5f47c
edorka <edorka@fd8f7eb6-0621-0410-b421-4720bd429cc4>
2007-02-07 12:01:12 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/main (copia).py

---
+++ src/main (copia).py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# main.py
+# Copyright (C) Eduardo Orive y Jose Antonio Martin 2006 <edorka@gmail.com jmartin23@gmail.com>
+# 
+# main.py is free software.
+# 
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option)
+# any later version.
+# 
+# main.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with main.py.  If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA  02110-1301, USA.
+
+
+print "Hello World!"
